dd681d8bdd21010842b762ff7b1b746285bf2b11
zananpech <zananpech9@gmail.com>
2023-03-19 13:52:10 +0100

    Add purchase info class

---
+++ src/main/java/com/thesis/vrbackend/model/PurchaseInfo.java
@@ -0,0 +1,29 @@
+package com.thesis.vrbackend.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class PurchaseInfo extends BaseEntity{
+
+  @NotNull
+  private Long userId;
+
+  @NotNull
+  private Long productId;
+
+  @NotNull
+  private String price;
+
+  @NotNull
+  private String productName;
+}
